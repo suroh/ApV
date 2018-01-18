2e58cf2ecb1a26314799f3571e4338dcfce12d85
suroh <Aaronjohnson@protonmail.ch>
2018-01-18 18:45:41 -0500

    Add files via upload

--- Re-write/New (QT)/qt-apv.py
+++ Re-write/New (QT)/qt-apv.py
@@ -6,8 +6,130 @@ from PyQt5.QtGui import QIcon
 from PyQt5.QtCore import pyqtSlot
 from PyQt5.QtGui import QFont
 
+class ApvSetup(object):
+
+    def __init__(self):
+        self.__linux = '/home/' + str(os.getlogin()) + '/APV/'
+        self.__winblows = 'C:\\APV\\'
+        self.__mac = '/home/' + str(os.getlogin()) + '/.APV/'
+        self.verify_setup()
+
+    def verify_setup(self):
+        try:
+            if sys.platform == 'linux2':
+                if os.path.isdir(self.__linux):
+                    print('Directory: ' + self.__linux + 'exists aborting process')
+                elif not os.path.isdir(self.__linux):
+                    self.create_folder(self.__linux)
+                    self.create_excel_document(self.__linux + 'Private.xlsx')
+            elif sys.platform == 'win32':
+                if os.path.isdir(self.__winblows):
+                    print('Directory: ' + self.__winblows + 'exists aborting process')
+                elif not os.path.isdir(self.__winblows):
+                    self.create_folder(self.__winblows)
+                    self.create_excel_document(self.__winblows + 'Private.xlsx')
+            elif sys.platform == 'darwin':
+                if os.path.isdir(self.__mac):
+                    print('Directory: ' + self.__mac + 'exists aborting process')
+                elif not os.path.isdir(self.__mac):
+                    self.create_folder(self.__mac)
+                    self.create_excel_document(self.__mac + 'Private.xlsx')
+        except OSError as exception:
+            raise OSError('%s: %s' % (exception.strerror))
+        return None
+
+    def create_folder(self, path):
+        try:
+            if os.path.isdir(path):
+                pass
+            else:
+                os.makedirs(path)
+                print(path + ': has been created')
+        except IOError as exception:
+            raise IOError('%s: %s' % (path, exception.strerror))
+        return None
+
+    def create_excel_document(self, path):
+        try:
+            if sys.platform == 'linux2':
+                if os.path.isdir(path):
+                    if os.path.isfile(path + 'Private.xlsx'):  # .ods for linux so this should be changed
+                        pass
+                    elif not os.path.isfile(path):
+                        df1 = pd.DataFrame({'Listing': ['Example Listing'], 'Website': ['www.examplewesbite.com'],
+                                            'Email': ['ExampleEmail@email.com'], 'Username': ['ExampleUserName'],
+                                            'Password': ['ExamplePassword1234']})
+                        writer = pd.ExcelWriter(path, engine='xlsxwriter')
+                        df1.to_excel(writer, sheet_name='Accounts')
+                        workbook = writer.book
+                        worksheet1 = writer.sheets['Accounts']
+                        worksheet1.set_column(1, 5, 35)  #
+                        writer.save()
+                elif not os.path.isdir(path):
+                    df1 = pd.DataFrame({'Listing': ['Example Listing'], 'Website': ['www.examplewesbite.com'],
+                                        'Email': ['ExampleEmail@email.com'], 'Username': ['ExampleUserName'],
+                                        'Password': ['ExamplePassword1234']})
+                    writer = pd.ExcelWriter(path, engine='xlsxwriter')
+                    df1.to_excel(writer, sheet_name='Accounts')
+                    workbook = writer.book
+                    worksheet1 = writer.sheets['Accounts']
+                    worksheet1.set_column(1, 5, 35)  #
+                    writer.save()
+            elif sys.platform == 'win32':
+                if os.path.isdir(path):
+                    if os.path.isfile(path + 'Private.xlsx'):
+                        pass
+                    elif not os.path.isfile(path):
+                        df1 = pd.DataFrame({'Listing': ['Example Listing'], 'Website': ['www.examplewesbite.com'],
+                                            'Email': ['ExampleEmail@email.com'], 'Username': ['ExampleUserName'],
+                                            'Password': ['ExamplePassword1234']})
+                        writer = pd.ExcelWriter(path, engine='xlsxwriter')
+                        df1.to_excel(writer, sheet_name='Accounts')
+                        workbook = writer.book
+                        worksheet1 = writer.sheets['Accounts']
+                        worksheet1.set_column(1, 5, 35)
+                        writer.save()
+                elif not os.path.isdir(path):
+                    df1 = pd.DataFrame({'Listing': ['Example Listing'], 'Website': ['www.examplewesbite.com'],
+                                        'Email': ['ExampleEmail@email.com'], 'Username': ['ExampleUserName'],
+                                        'Password': ['ExamplePassword1234']})
+                    writer = pd.ExcelWriter(path, engine='xlsxwriter')
+                    df1.to_excel(writer, sheet_name='Accounts')
+                    workbook = writer.book
+                    worksheet1 = writer.sheets['Accounts']
+                    worksheet1.set_column(1, 5, 35)
+                    writer.save()
+            elif sys.platform == 'darwin':
+                if os.path.isdir(path):
+                    if os.path.isfile(path + 'Private.xlsx'):  # .ods for mac so this should be changed
+                        pass
+                    elif not os.path.isfile(path):
+                        df1 = pd.DataFrame({'Listing': ['Example Listing'], 'Website': ['www.examplewesbite.com'],
+                                            'Email': ['ExampleEmail@email.com'], 'Username': ['ExampleUserName'],
+                                            'Password': ['ExamplePassword1234']})
+                        writer = pd.ExcelWriter(path, engine='xlsxwriter')
+                        df1.to_excel(writer, sheet_name='Accounts')
+                        workbook = writer.book
+                        worksheet1 = writer.sheets['Accounts']
+                        worksheet1.set_column(1, 5, 35)
+                        writer.save()
+                elif not os.path.isdir(path):
+                    df1 = pd.DataFrame({'Listing': ['Example Listing'], 'Website': ['www.examplewesbite.com'],
+                                        'Email': ['ExampleEmail@email.com'], 'Username': ['ExampleUserName'],
+                                        'Password': ['ExamplePassword1234']})
+                    writer = pd.ExcelWriter(path, engine='xlsxwriter')
+                    df1.to_excel(writer, sheet_name='Accounts')
+                    workbook = writer.book
+                    worksheet1 = writer.sheets['Accounts']
+                    worksheet1.set_column(1, 5, 35)
+                    writer.save()
+        except IOError as exception:
+            raise IOError('%s: %s' % (exception.strerror))
+        return None
+
 
 class Apv(QMainWindow):
+
     def __init__(self):
         super().__init__()
         self.title = 'Apv Alpha .5'
@@ -15,20 +137,30 @@ class Apv(QMainWindow):
         self.top = 10
         self.width = 800
         self.height = 650
-        self.initUI()
-        
-    def initUI(self):
+
+
+
+        ApvSetup().verify_setup()
+        self.__master_dict = {'Listing': None, 'Website': None, 'Email': None, 'Username': None, 'Password': None}
+        self.__linux = '/home/' + str(os.getlogin()) + '/APV/'
+        self.__winblows = 'C:\\APV\\'
+        self.__mac = '/home/' + str(os.getlogin()) + '/.APV/'
+
+        self.init_ui()
+
+    def init_ui(self):
         self.setWindowTitle(self.title)
         self.setGeometry(self.left, self.top, self.width, self.height)
         '''
          QText Edits
         '''
         
-        listing_gui = QTextEdit('', self)
-        listing_gui.setFont(QFont('Arial', 12))
-        listing_gui.setPlaceholderText('Listing: ')
-        listing_gui.resize(200, 30)
-        listing_gui.move(0, 30)
+        self.listing_gui = QLineEdit('', self)
+        self.listing_gui.setFont(QFont('Arial', 12))
+        self.listing_gui.setPlaceholderText('Listing: ')
+        self.listing_gui.resize(200, 30)
+        self.listing_gui.move(0, 30)
+        self.listing_gui.returnPressed.connect(self.get_listing)
         
         #width = listing_gui.frameGeometry().width()
         #height = listing_gui.frameGeometry().height()
@@ -36,29 +168,33 @@ class Apv(QMainWindow):
         #print(height)
         
         
-        website_gui = QTextEdit('', self)
-        website_gui.setFont(QFont('Arial', 12))
-        website_gui.setPlaceholderText('Website: ')
-        website_gui.resize(200, 30)
-        website_gui.move(0, 60)
+        self.website_gui = QLineEdit('', self)
+        self.website_gui.setFont(QFont('Arial', 12))
+        self.website_gui.setPlaceholderText('Website: ')
+        self.website_gui.resize(200, 30)
+        self.website_gui.move(0, 60)
+        self.website_gui.returnPressed.connect(self.get_website)
         
-        email_gui = QTextEdit('', self)
-        email_gui.setFont(QFont('Arial', 12))
-        email_gui.setPlaceholderText('Email Address: ')
-        email_gui.resize(200, 30)
-        email_gui.move(0, 90)
+        self.email_gui = QLineEdit('', self)
+        self.email_gui.setFont(QFont('Arial', 12))
+        self.email_gui.setPlaceholderText('Email Address: ')
+        self.email_gui.resize(200, 30)
+        self.email_gui.move(0, 90)
+        self.email_gui.returnPressed.connect(self.get_email)
         
-        username_gui = QTextEdit('', self)
-        username_gui.setFont(QFont('Arial', 12))
-        username_gui.setPlaceholderText('Username: ')
-        username_gui.resize(200, 30)
-        username_gui.move(0, 120)
+        self.username_gui = QLineEdit('', self)
+        self.username_gui.setFont(QFont('Arial', 12))
+        self.username_gui.setPlaceholderText('Username: ')
+        self.username_gui.resize(200, 30)
+        self.username_gui.move(0, 120)
+        self.username_gui.returnPressed.connect(self.get_username)
         
-        password_gui = QTextEdit('', self)
-        password_gui.setFont(QFont('Arial', 12))
-        password_gui.setPlaceholderText('Password: ')
-        password_gui.resize(200, 30)
-        password_gui.move(0, 150)
+        self.password_gui = QLineEdit('', self)
+        self.password_gui.setFont(QFont('Arial', 12))
+        self.password_gui.setPlaceholderText('Password: ')
+        self.password_gui.resize(200, 30)
+        self.password_gui.move(0, 150)
+        self.password_gui.returnPressed.connect(self.get_password)
         
         '''
          End QText Edits
@@ -79,7 +215,6 @@ class Apv(QMainWindow):
         
         dock = QDockWidget('Table', self)
         dock.setWidget(table)
-        #dock.setLayout(QVBoxLayout())
         dock.resize(800, 650 - 200)
         dock.move(0, 180)
         
@@ -153,7 +288,54 @@ class Apv(QMainWindow):
          End Menu
         '''
         self.show()
- 
+
+    def get_listing(self):
+        __listing = self.listing_gui.text()
+        self.__master_dict['Listing'] = __listing
+
+    def get_website(self):
+        __website = self.website_gui.text()
+        self.__master_dict['Website'] = __website
+
+    def get_email(self):
+        __email = self.email_gui.text()
+        self.__master_dict['Email'] = __email
+
+    def get_username(self):
+        __usr = self.username_gui.text()
+        self.__master_dict['Username'] = __usr
+
+    def get_password(self):
+        if sys.platform == 'linux2':
+            __password = self.password_gui.text()
+            self.__master_dict['Password'] = __password
+            self.append_and_write_dataframes(self.__linux)
+        elif sys.platform == 'win32':
+            __password = self.password_gui.text()
+            self.__master_dict['Password'] = __password
+            self.append_and_write_dataframes(self.__winblows)
+        elif sys.platform == 'darwin':
+            __password = self.password_gui.text()
+            self.__master_dict['Password'] = __password
+            self.append_and_write_dataframes(self.__mac)
+
+    def append_and_write_dataframes(self, path):
+        df = pd.read_excel(path, sheet_name='Accounts')
+        df1 = pd.DataFrame({'Listing': [self.__master_dict['Listing']], 'Website': [self.__master_dict['Website']],
+                            'Email': [self.__master_dict['Email']], 'Username': [self.__master_dict['Username']],
+                            'Password': [self.__master_dict['Password']]})
+        df3 = df.append(df1)
+        df3 = df3[['Listing', 'Website', 'Email', 'Username', 'Password']]
+        writer = pd.ExcelWriter(path, engine='xlsxwriter')
+        df3.to_excel(writer, sheet_name='Accounts')
+        workbook = writer.book
+        worksheet1 = writer.sheets['Accounts']
+        worksheet1.set_column(1, 5, 35)
+        writer.save()
+        self.__master_dict.clear()
+
+
+
 if __name__ == '__main__':
     apv = QApplication(sys.argv)
     ex = Apv()
--- Re-write/New (QT)/setup.py
+++ Re-write/New (QT)/setup.py
@@ -2,8 +2,7 @@ import sys
 import os
 import pandas as pd
 
-'''
- 
+
 class ApvCore(object):
     
     def __init__(self):
@@ -126,126 +125,7 @@ class ApvCore(object):
         return None
     
     
-c = ApvCore()
-c.verify_setup()
-'''
-
-linux = '/home/' + os.getlogin() + '/.APV'
-winblows = 'C:\\APV\\'
-mac = '/home/' + str(os.getlogin()) + '/.APV/'
+ApvCore().verify_setup()
 
 
-def verify_setup():
-    try:
-        if sys.platform == 'linux2':
-            if os.path.isdir(linux):
-                print('Directory: ' + linux + 'exists aborting process')
-            elif not os.path.isdir(linux):
-                create_folder(linux)
-                create_excel_document(linux + 'Private.xlsx')
-        elif sys.platform == 'win32':
-            if os.path.isdir(winblows):
-                print('Directory: ' + winblows + 'exists aborting process')
-            elif not os.path.isdir(winblows):
-                create_folder(winblows)
-                create_excel_document(winblows + 'Private.xlsx')
-        elif sys.platform == 'darwin':
-            if os.path.isdir(mac):
-                print('Directory: ' + mac + 'exists aborting process')
-            elif not os.path.isdir(mac):
-                create_folder(mac)
-                create_excel_document(mac + 'Private.xlsx')
-    except OSError as exception:
-        raise OSError('%s: %s' % (exception.strerror))
-    return None
-
-def create_folder(path):
-    try:
-        if os.path.isdir(path):
-            pass
-        else:
-            os.makedirs(path)
-            print(path + ': has been created')
-    except IOError as exception:
-        raise IOError('%s: %s' % (path, exception.strerror))
-    return None
-
-def create_excel_document(self, path):
-    try:
-        if sys.platform == 'linux2':
-            if os.path.isdir(path):
-                if os.path.isfile(path + 'Private.xlsx'):  # .ods for linux so this should be changed
-                    pass
-                elif not os.path.isfile(path):
-                    df1 = pd.DataFrame({'Listing': ['Example Listing'], 'Website': ['www.examplewesbite.com'],
-                                        'Email': ['ExampleEmail@email.com'], 'Username': ['ExampleUserName'],
-                                        'Password': ['ExamplePassword1234']})
-                    writer = pd.ExcelWriter(path, engine='xlsxwriter')
-                    df1.to_excel(writer, sheet_name='Accounts')
-                    workbook = writer.book
-                    worksheet1 = writer.sheets['Accounts']
-                    worksheet1.set_column(1, 5, 35)  #
-                    writer.save()
-                elif not os.path.isdir(path):
-                    df1 = pd.DataFrame({'Listing': ['Example Listing'], 'Website': ['www.examplewesbite.com'],
-                                        'Email': ['ExampleEmail@email.com'], 'Username': ['ExampleUserName'],
-                                        'Password': ['ExamplePassword1234']})
-                    writer = pd.ExcelWriter(path, engine='xlsxwriter')
-                    df1.to_excel(writer, sheet_name='Accounts')
-                    workbook = writer.book
-                    worksheet1 = writer.sheets['Accounts']
-                    worksheet1.set_column(1, 5, 35)  #
-                    writer.save()
-            elif sys.platform == 'win32':
-                if os.path.isdir(path):
-                    if os.path.isfile(path + 'Private.xlsx'):
-                        pass
-                    elif not os.path.isfile(path):
-                        df1 = pd.DataFrame({'Listing': ['Example Listing'], 'Website': ['www.examplewesbite.com'],
-                                            'Email': ['ExampleEmail@email.com'], 'Username': ['ExampleUserName'],
-                                            'Password': ['ExamplePassword1234']})
-                        writer = pd.ExcelWriter(path, engine='xlsxwriter')
-                        df1.to_excel(writer, sheet_name='Accounts')
-                        workbook = writer.book
-                        worksheet1 = writer.sheets['Accounts']
-                        worksheet1.set_column(1, 5, 35)
-                        writer.save()
-                elif not os.path.isdir(path):
-                    df1 = pd.DataFrame({'Listing': ['Example Listing'], 'Website': ['www.examplewesbite.com'],
-                                        'Email': ['ExampleEmail@email.com'], 'Username': ['ExampleUserName'],
-                                        'Password': ['ExamplePassword1234']})
-                    writer = pd.ExcelWriter(path, engine='xlsxwriter')
-                    df1.to_excel(writer, sheet_name='Accounts')
-                    workbook = writer.book
-                    worksheet1 = writer.sheets['Accounts']
-                    worksheet1.set_column(1, 5, 35)
-                    writer.save()
-            elif sys.platform == 'darwin':
-                if os.path.isdir(path):
-                    if os.path.isfile(path + 'Private.xlsx'):  # .ods for mac so this should be changed
-                        pass
-                    elif not os.path.isfile(path):
-                        df1 = pd.DataFrame({'Listing': ['Example Listing'], 'Website': ['www.examplewesbite.com'],
-                                            'Email': ['ExampleEmail@email.com'], 'Username': ['ExampleUserName'],
-                                            'Password': ['ExamplePassword1234']})
-                        writer = pd.ExcelWriter(path, engine='xlsxwriter')
-                        df1.to_excel(writer, sheet_name='Accounts')
-                        workbook = writer.book
-                        worksheet1 = writer.sheets['Accounts']
-                        worksheet1.set_column(1, 5, 35)
-                        writer.save()
-                elif not os.path.isdir(path):
-                    df1 = pd.DataFrame({'Listing': ['Example Listing'], 'Website': ['www.examplewesbite.com'],
-                                        'Email': ['ExampleEmail@email.com'], 'Username': ['ExampleUserName'],
-                                        'Password': ['ExamplePassword1234']})
-                    writer = pd.ExcelWriter(path, engine='xlsxwriter')
-                    df1.to_excel(writer, sheet_name='Accounts')
-                    workbook = writer.book
-                    worksheet1 = writer.sheets['Accounts']
-                    worksheet1.set_column(1, 5, 35)
-                    writer.save()
-    except IOError as exception:
-        raise IOError('%s: %s' % (exception.strerror))
-    return None
-
-verify_setup()
+# The installer is depricated
